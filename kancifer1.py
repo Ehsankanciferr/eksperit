53bb18877a9c7694b043b29a39ee252241f0e76b
Ehsankanciferr <98548114+Ehsankanciferr@users.noreply.github.com>
2022-01-28 03:54:23 +0330

    Create kancifer1.py

---
+++ kancifer1.py
@@ -0,0 +1,278 @@
+# MAMAD RYSON *
+# Khasteh Az Hameh
+print("")
+print("")
+print("ehsan kancifer")
+print (" ")
+print("Load . . .")                 
+import os
+#What do you have in the source?
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+
+
+os.system("clear")
+
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+print ("Note that this expression is currently old")
+print (" ")
+#date
+x = str(datetime.datetime.now())
+
+print("Beginning " + (x))
+
+print(f"{blue} ")
+print ("         GOD FILTERING")
+print ("   They are the only creators of (Ryson) Mammad")
+print ("""The best filtering rule in virtual: (law-ryson.blogfa.com There are Ryson! rules.""")
+print (Fore.YELLOW + "")
+print (""" Supermarket code reporter in virtual by mmd """)
+print (Fore.GREEN + "")
+x = """ Loading ~ ~ ~ 
+.                                                      .
+      .n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP                      9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP                                ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(                                                        ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb                                                   9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP                                                                     XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX                                                                       X. 9         P )X
+                         b          '  d   """
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+print(f"{red} ")
+d = """
+I Lobe You-
+Manufacturer- kancifer
+                            kamcifer
+         GOD FILTERING RUBIKA kancifer
+/~MMD ~> ryson /-/-/
+                    CODE FILTERI RUBIKA
+             EhSaM kancifer 💗
+     WEBLOG GHAVANIN ~> kancifer.Com
+          
+          Search Google "ehsan kancifer" Please!
+FILTER RUBIKA ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||_ _ _ _ _
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+
+# Chaker * _ *  Hal Kardi?
+
+time.sleep(2)
+print(f"{green} ")
+print ("Play - - - ! ")
+print (Fore.BLUE + "")
+time.sleep(1.7)
+print ("                     off rubika.ir/kancifer")
+print("                  Fil rubika.ir/kancifer")
+print ("                telegram /Y_termux_N")
+print ("             WebLog ghavanin : ||||||||||||||~ ")
+print ("")
+print(f"{blue} ")
+time.sleep(0.6)
+print ("               !")
+time.sleep(0.2)
+print ("               !!")
+time.sleep(1.0)
+print ("              !!!!")
+time.sleep(0.6)
+print ("             !!!!!")
+time.sleep(0.2)
+print ("            !!!!!!")
+time.sleep(0.6)
+print ("           !!!!!!!!!")
+time.sleep(0.4)
+print ("          !!!!!!!!!!!")
+time.sleep(0.4)
+print ("         !!!!!!!!!!!!!!")
+time.sleep(0.3)
+print ("        !!!!!!!!!!!!!!!!")
+time.sleep(0.2)
+print ("       !!!!!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print ("      !!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.5)
+print ("     !!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.4)
+print ("    !!!!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.3)
+print ("   !!!!!!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.2)
+print ("   !!!!!!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print ("    !!!!!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print ("      !!!!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.3)
+print ("       !!!!!!!!!!!!!!!!!!!!")
+time.sleep(0.2)
+print ("        !!!!!!!!!!!!!!!!!!")
+time.sleep(0.2)
+print ("         !!!!!!!!!!!!!!!!")
+time.sleep(0.1)
+print ("          !!!!!!!!!!!!!!")
+time.sleep(0.3)
+print ("           !!!!!!!!!!!!")
+time.sleep(0.4)
+print ("            !!!!!!!!!!")
+time.sleep(0.3)
+print ("             !!!!!!!!")
+time.sleep(0.2)
+print ("              !!!!!!")
+time.sleep(0.1)
+print ("               !!!!")
+time.sleep(1.0)
+print ("                !!")
+time.sleep(0.3)
+print ("                 EE")
+time.sleep(0.3)
+print ("                 R")
+time.sleep(0.2)
+print ("")
+print (" ")
+print ("")
+# 1 chanel 2 Group 3 acc
+up = input(""" 
+[Chanel   (1) ]
+[Grup     (2) ]
+                 
+[Account  (3) ]
+                  
+[Super    (0) ]
+[Exit     (4) ]
+[Code Pd  (5) ]
+[Code Rp  (6) ]
+[reporter (7) ]
+[sms bomb (8) ]
+Which do you want>>> """)
+print("")
+print(f"{pink} ")
+time.sleep(3.0)
+print(""" in the name of God 
+_____________
+ _  _
+ 
+💣 - 💣
+                      🇴‌🇵‌🇪‌🇳‌ - 🇷‌🇪‌🇵‌🇴‌🇷‌🇹‌
+    
+||Web: creator-rayson.blogfa.com||
+|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>|>||
+________rubika.ir/Caetorr________
+""")
+print(f"{red} ")
+
+if up == "8":
+    os.system("python sms.py")
+if up == "7":
+    os.system("python rep.py")
+if up == "6":
+    os.system("python report.py")
+if up == "5":
+    os.system("python pass.py")
+if up == "4":
+    os.system("exit 8")
+    
+if up == "0":
+         print ('(Super) Code {filter} =>' + '%s%s (4.0.4.5/y//d/f/4.0.8.9.4.0.1.5.7.8.0.1.3.0.1.5.6.0.1.3) %s' % (fg('red'), bg('black'), attr('reset')))
+
+
+if up == "1":
+
+        print ('code {Filter} (channel) ≠>' + '%s%s (7.0.6.4/f//d.5.2.7.9.0.9.1.3.5.0.3.1.7.8.9.2.6.0.3.8.4.3.0.2) %s' % (fg('red'), bg('green'), attr('reset')))
+
+
+if up != "1":
+        print("    COD  ")
+
+
+if up == "2":
+        print ('code {Filter} (group) ≠>' + '%s%s (9.0.3.7/f////g.h/5.2.7.9.5.0.1.2.6.0.8.1.7.0.7.3.3.8.6.4) %s' % (fg('white'), bg('yellow'), attr('reset')))
+
+
+if up != "2":
+        print("     cσ∂s мү яερσят ")
+
+
+if up == "3":
+
+        print ('code {Filter} (user) ≠>' + '%s%s (4.0.7.5/f//a/y/7.0.9.3.6.0.3.2.5.3.6.8.5.0.4.9.4.0.3.6.09/) %s' % (fg('red'), bg('blue'), attr('reset')))
+
+
+if up != "3":
+        print("""
+        
+             Khalegh Ryson""")
+
+time.sleep(0.6)
+print(f"{green}" )
+print ("""
+Kanal rubika EHSAN KANCIFER =
+@kancifer
+ """)
+print (" Kanal rubika =>> https://rubika.ir/joinc/JDDGCJF0YETLWSRZLYDCJWIOBMCANFKJ ")
+print(f"{blue}" )
+print(" ")
+print(" Web Cod = Code-Filtring.Blogfa.Com")
+print("")
+print(" Bot Tel Cod = @Code_Filtering_bot  &&  @Spansor_Bot }")
+print("")
+print(" server= ///g///h ")
+print("")
+print(" Chanel Telegram=>> @Uupdatte -")
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print (" Ostoreh Filtering All EHSAN KANCIFER")
+time.sleep(3.0)
+print (f"{red} ")
+time.sleep(1)
+print ('Cod Ro Dorost Bezan 70% Filter Mikone  ' + '%s%s TanK You My Friend  %s'  % (fg('red'), bg('white'), attr('reset')))
+print (f"{blue} ")
+time.sleep(1)
+print (""" {code ro ( 5 ) bar bezanin to Sayer!!}
+         Tanks""")
+print(" ")
+print(" ")
+print(" ")
+print ("Note that this expression is currently old")
+print("")
+print(""" IM EHSAN KANCIFER
+""")
+print("")
+print("")
+time.sleep(0.3)
+print(f"{red}" )
+time.sleep(0.5)
+print("                                <♥️/>")
+print (Fore.GREEN + """ 
+           #666 Super Script 
+                 Finished Code """)
+     
